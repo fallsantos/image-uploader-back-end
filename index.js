6dfe901a21f30fa125d91a7b07be3116cefd02a9
Flavio Santos <fall-_santos@hotmail.com>
2019-10-17 17:58:08 -0300

    Change PORT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app/config/server')
 
-const PORT = app.get('port')
+//const PORT = app.get('port')
 
-app.listen(PORT || 3000, () => {
+app.listen(procces.env.PORT || 3000, () => {
     console.info(`Server running on port ${PORT}...`)
 })
